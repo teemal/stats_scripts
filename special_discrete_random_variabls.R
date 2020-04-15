2d1e3fe4658d26c3f5413d1db70c5304a49d1ca3
teemal <taymal1987@gmail.com>
2020-04-14 19:31:00 -0700

    halfway through homework 3

--- special_discrete_random_variabls.R
+++ special_discrete_random_variabls.R
@@ -1,3 +1,17 @@
+#1
 x <- 0:2
-p_x <- dbinom(x, 4, 1/3)
+p_x <- dhyper(x, 5, 10, 4)
 print(sum(p_x))
+
+#3
+x <- 0:10
+p_x <- dhyper(x, 60, 40, 10)
+dist <- data.frame(x, p_x)
+print(p_x)
+
+
+#6
+exp_val <- 1*p_x[2] + 2*p_x[3] + 3*p_x[4] + 4*p_x[5] + 5*p_x[6] + 6*p_x[7] + 7*p_x[8] + 8*p_x[9] + 9*p_x[10] + 10*p_x[11]
+print(exp_val)
+
+
