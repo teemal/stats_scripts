4cc8376ab6ba7729ee44ad581d2cfb207e7b1020
teemal <taymal1987@gmail.com>
2020-04-28 11:03:13 -0700

    normal distribution stuff

--- special_discrete_random_variabls.R
+++ special_discrete_random_variabls.R
@@ -11,14 +11,13 @@ p_x <- dbinom(x, 10, .6)
 dist <- data.frame(x, p_x)
 dist
 
-#4
-
-
-
 #6
 exp_val <- 1*p_x[2] + 2*p_x[3] + 3*p_x[4] + 4*p_x[5] + 5*p_x[6] + 6*p_x[7] + 7*p_x[8] + 8*p_x[9] + 9*p_x[10] + 10*p_x[11]
 print(exp_val)
 
+#7
+v_x <- (0- exp_val)**2*p_x[1] + (1- exp_val)**2*p_x[2] + (2- exp_val)**2*p_x[3] + (3- exp_val)**2*p_x[4] + (4- exp_val)**2*p_x[5] + (5- exp_val)**2*p_x[6] + (6- exp_val)**2*p_x[7] + (7- exp_val)**2*p_x[8] + (8- exp_val)**2*p_x[9] + (9- exp_val)**2*p_x[10] + (10- exp_val)**2*p_x[11])
+v_x
 #8
 x <- 0:5
 p_x <- dhyper(x, 3, 7, 5)
@@ -29,7 +28,7 @@ x<-0:16
 p_x = dbinom(x, 16, .22)
 dist<-data.frame(x,p_x)
 dist
-print(sum(p_x[5:16]))
+print(1 - sum(p_x[1:4]))
 
 
 #12
@@ -37,7 +36,12 @@ sum(dpois(0:1, 1.533))
 
 
 #13 ?
-
+x<-dpois(0:30, 10)
+x
+plot(x ~ x)
 
 #15
+y <- 1 - sum(x[1:11])
+y
 
+x[1:11]
