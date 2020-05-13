0a8ad990c12969f74bd72097e633525e3fa4d647
teemal <taymal1987@gmail.com>
2020-05-13 09:50:03 -0700

    finished #2, #3 is not completely correct

--- assignment_6.R
+++ assignment_6.R
@@ -1,4 +1,7 @@
-#1
+#1 
+#Use R to generate 5000 random samples of sizen= 252 of city mileage from cars withmanualtransmissionsonly, 
+#and compute the statistic ̄Xfrom each sample, the average city mileage of cars with manual transmis-sions. 
+#Then create a histogram of those 5000 statistics.  Include a description of the shape, the center, andthe spread of the distribution.
 avg_mpg <- 0
 
 for(i in 1:5000){
@@ -8,3 +11,11 @@ for(i in 1:5000){
 
 hist(avg_mpg)
 summary(avg_mpg)
+
+#2
+#What is the standard error of the sampling distribution in #1?
+sd(avg_mpg)
+
+#3
+#Use the Central Limit Theorem to approximate the probability that the average city mileage in a sample of252 cars with manual transmissions is greater than 21 MPG.
+pnorm(21, mean(avg_mpg), sd(avg_mpg)/sqrt(252))
